5260ac111fc1445dd75d537fccd766031c1c40b3
anothergrind <ozorjikj@gmail.com>
2025-06-08 18:00:59 -0500

    adjusting look of the book/id page to fit figma wireframe

--- canvas/src/app/book/[id]/page.tsx
+++ canvas/src/app/book/[id]/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import styles from '../../styles/book.module.css';
 
@@ -18,42 +17,41 @@ export default async function BookDetails({ params }: { params: { id: string } }
   return (
     <Box className={styles['book-details-container']}>
       <Typography variant="h3" className={styles['book-title']} gutterBottom>
-        {book.title}
+        Title: {book.title}
       </Typography>
       <Typography variant="h5" className={styles['book-author']}>
-        {book.author}
+        Author: {book.author}
       </Typography>
       <Typography variant="subtitle1" className={styles['book-rating']} gutterBottom>
-        {book.rating}
+        Rating: {book.rating}
       </Typography>
-      <Grid container spacing={4} className={styles['book-details-grid']}>
+      <Box className={styles['book-details-grid']}>
         <Box className={styles['book-image-box']}>
-          {/* Replace with actual image if available */}
-          <Typography>IMAGE OF THE BOOK</Typography>
+          {book.cover_image ? (
+            <img
+              src={book.cover_image}
+              alt={book.title}
+              style={{ maxWidth: '100%', maxHeight: '100%', borderRadius: 12 }}
+            />
+          ) : (
+            <Typography>IMAGE OF THE BOOK</Typography>
+          )}
         </Box>
         <Box className={styles['book-description-box']}>
-          <Typography fontWeight="bold">{book.description}</Typography>
+          <Typography>{book.description}</Typography>
         </Box>
-      </Grid>
-      <Grid container spacing={2} className={styles['book-actions-grid']} justifyContent="center">
-        <Box>
-          <Button variant="outlined">View Sample of {book.title}</Button>
-        </Box>
-        <Box>
-          <Button variant="outlined">View Published version of {book.title}</Button>
-        </Box>
-        <Box>
-          <Button variant="outlined">View Reviews of {book.title}</Button>
-        </Box>
-        <Box>
-          <Button variant="outlined">Share {book.title}</Button>
-        </Box>
-        <Box className={styles['book-community-btn-box']}>
-          <Button variant="outlined" className={styles['book-community-btn']}>
-            View Community
-          </Button>
-        </Box>
-      </Grid>
+      </Box>
+      <Box className={styles['book-actions-grid']}>
+        <Button variant="outlined">VIEW SAMPLE OF {book.title.toUpperCase()}</Button>
+        <Button variant="outlined">VIEW PUBLISHED VERSION OF {book.title.toUpperCase()}</Button>
+        <Button variant="outlined">VIEW REVIEWS OF {book.title.toUpperCase()}</Button>
+        <Button variant="outlined">SHARE {book.title.toUpperCase()}</Button>
+      </Box>
+      <Box className={styles['book-community-btn-box']}>
+        <Button variant="outlined" className={styles['book-community-btn']}>
+          VIEW COMMUNITY
+        </Button>
+      </Box>
     </Box>
   );
 }
